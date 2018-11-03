5db46a46ef623af0aeda9b824cd43ba79b9144f4
Geoffrey Challen <challen@illinois.edu>
2018-11-03 17:35:18 -0500

    Remove unused import error.

--- lib/src/main/java/edu/illinois/cs/cs125/mp5/lib/MoleculeAnalyzer.java
+++ lib/src/main/java/edu/illinois/cs/cs125/mp5/lib/MoleculeAnalyzer.java
@@ -2,7 +2,6 @@ package edu.illinois.cs.cs125.mp5.lib;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
